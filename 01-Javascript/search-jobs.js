bda2949974be7c8305859b3e685cdb3ccea3ee36
yorsHenao <yorshenao.gmail.com>
2026-05-01 22:31:19 -0500

    funcion_filtro_search

--- 01-Javascript/search-jobs.js
+++ 01-Javascript/search-jobs.js
@@ -1,7 +1,23 @@
 //busqueda empleados
 
 const searchForm = document.querySelector("#empleos-search-form")
+const searchInput = searchForm.querySelector("input")
 
 searchForm.addEventListener("submit", () => {
-    event.preventDefault()
+    event.preventDefault() // previene que la pagina se recargue cuando el usuario hace submit
+    
+    const tarjeta_seleccionada = document.querySelectorAll(".job-listing-card")
+    const valor_input = searchInput.value.toLowerCase() // convierte texto en minuscula
+
+    tarjeta_seleccionada.forEach(tarjeta => {
+        const descripcion = tarjeta.textContent.toLowerCase()//-> textContent devuelve todo el texto visible del elemento como un string
+       
+        //¿el usuario no escribio nada? o ¿o la tarjeta coincide contiene lo que busco
+        const isShow = valor_input === "" || descripcion.includes(valor_input)
+        tarjeta.classList.toggle("is-hidden", !isShow)
+    })
 })
+
+
+
+
