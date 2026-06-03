831c682d896d2ebe9e5d89202f3684ce8db77c74
yorsHenao <yorshenao.gmail.com>
2026-06-02 19:20:33 -0500

    sacamos current de useRouter

--- 02-react/src/componets/link.jsx
+++ 02-react/src/componets/link.jsx
@@ -1,14 +1,23 @@
 import { useRouter } from "../hooks/useRouter.jsx";
 
 export function Link({href, children, ...restOfProps}) {
-    const { navigateTo } = useRouter();
+    const { navigateTo, currentPath } = useRouter(); //navigato para cambiar de pagina y currentPath para saber en que pagina estoy ahora
+                                                
+
+    const isActive = currentPath === href;
+    //si la pagina actual es igual al href del link, entonces el link esta activo
+    //si no false
+
+
     const handleClick = (event) => {
         event.preventDefault();
         navigateTo(href);
     }
 
     return (
-        <a href={href} {...restOfProps} onClick={handleClick}>
+        <a href={href} {...restOfProps} 
+        onClick={handleClick}
+        className={isActive ? "active" : ""}>
         {children}
         </a>
     )
--- 02-react/src/pages/Contact.jsx
+++ 02-react/src/pages/Contact.jsx
@@ -1,10 +1,46 @@
+import {useState} from "react";
 
 
 export function ContactPage() {
+    const [nombre, setNombre] = useState("")
+    const [apellido, setApellido] = useState("")
+    const [email, setEmail] = useState("")
+    const [mensaje, setMensaje] = useState("")
+
+    const handleSubmit = () => {
+        if (nombre === "") {
+            alert("Por favor ingresa tu nombre")
+            return
+        }
+        if (apellido === "") {
+            alert("Por favor ingresa tu apellido")
+            return
+        }
+        if (email === "") {
+            alert("Por favor ingresa tu email")
+            return
+        } else if (!email.includes("@")) {
+            alert("Por favor ingresa un email válido")
+            return
+        }
+        if (mensaje === "") {
+            alert("Por favor ingresa tu mensaje")
+            return 
+        }
+
+        alert("Mensaje enviado correctamente")
+    }
+
+    
     return (
         <main>
             <h2>Contacto</h2>
             <p>Si tienes alguna pregunta o sugerencia, no dudes en contactarnos</p>
+            <input value={nombre} onChange={(event) => setNombre(event.target.value)} placeholder="Nombre"/>
+            <input value={apellido} onChange={(event) => setApellido(event.target.value)} placeholder="Apellido"/>
+            <input value={email} onChange={(event) => setEmail(event.target.value)} placeholder="Email"/>
+            <input value={mensaje} onChange={(event) => setMensaje(event.target.value)} placeholder="Mensaje"/>
+            <button onClick={handleSubmit}>Enviar</button>
         </main>
     )
 }
